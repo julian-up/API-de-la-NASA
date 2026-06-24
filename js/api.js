f683e4b01eb7cb07f31a225baae3f8253bbdcec3
Julian Gonzalez <jgonzalezmal@outlook.com>
2026-06-24 10:04:21 -0500

    Desarrollo Api y Ui a cargo de JM

---
+++ js/api.js
@@ -0,0 +1,7 @@
+const API_KEY = "https://api.nasa.gov/planetary/apod?api_key=1TUKsecP1PIy1quX7VkbxAJuJwPHWXOD8DaJRNeO"
+
+export async function obtenerAPOD(fecha = "") {
+    let url = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`
+
+    let (fecha)
+}
